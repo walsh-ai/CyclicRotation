34f1edd6c37d7462416bc8d779626e376b7ca37e
csnirs <walsh.jthomas@outlook.com>
2023-08-30 17:47:49 +0100

    First commit, solution without base return case or exception checks

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Top level statements 
+// Test solution here 
+using System.Text;
+
+int[] arr = {3, 8, 9, 7, 6}; 
+int[] result = Rotate(arr, 3); 
+
+StringBuilder sb = new StringBuilder("{"); 
+foreach (int i in result) {
+    sb.Append(i.ToString()); 
+    sb.Append(", ");
+}
+sb.Append("}"); 
+
+Console.WriteLine($"Result of A=[3, 8, 9, 7, 6], K=3: {sb.ToString()}");
+
+public static partial class Program {
+    /// <summary>
+    /// Rotate the elements of array A, K times.
+    /// Rotation means that each element is moved to the right one index, such
+    /// that the last element of A becomes the first element. 
+    /// </summary>
+    /// <param name="a">Array on which to perform rotation</param>
+    /// <param name="k">Number of rotations to perform</param>
+    /// <returns></returns>
+    public static int[] Rotate(int[] a, int k) {
+        int temp, prev; 
+        while (k-- > 0) {
+            prev = a[1]; 
+            a[1] = a[0]; 
+
+            for (int i = 1; i < (a.Length - 1); i++) {
+                temp = a[i + 1]; 
+                a[i + 1] = prev;
+                prev = temp; 
+            }
+
+            a[0] = prev; 
+        }
+
+        return a; 
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+Rotate array A, K times. This means that each element of the array
+will be shifted to the right, K times. 
+
+
+The array consists of N integers. Rotation means that each element is moved right by one index, and the last element becomes the first element. 
+
+Given A = {3, 8, 9, 7, 6} and K = 3 the result of three rotations is 
+	A = {9, 7, 6, 3, 8}
+
+
